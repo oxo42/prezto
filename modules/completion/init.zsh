bb95a66fc3d0afbbd0a4e86b582fdaa089bbc5dc
John Oxley <joxley@fb.com>
2018-01-16 15:39:17 -0800

    .

--- modules/completion/init.zsh
+++ modules/completion/init.zsh
@@ -79,7 +79,7 @@ zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
 zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
-zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'expand'
 zstyle ':completion:*' squeeze-slashes true
 
 # History
